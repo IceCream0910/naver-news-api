4fbdbbd144b6b780a1d8fa90f092b33c4d4d07d3
Icecream0910 <taein2370@naver.com>
2022-06-07 23:43:46 +0900

    INIT

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+
+const client_id = "Rq8gv_hXQgJA56hdd7tL";
+const client_secret = "uiieuA1ni1";
+
+
+app.get('/search/news', function (req, res) {
+    var api_url = 'https://openapi.naver.com/v1/search/news?display=30&query=' + encodeURI(req.query.query); // json 결과
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+
+app.get('/trend', function (req, res) {
+
+    var api_url = 'https://www.bigkinds.or.kr/api/categoryKeywords.do' // json 결과
+    var request = require('request');
+    var options = {
+        url: api_url
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
